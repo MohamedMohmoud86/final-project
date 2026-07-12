c7291c0a6652a22ee02830629d7deec010df3235
MohamedMohmoud86 <r4rfredtre3@gmail.com>
2026-07-12 11:27:27 +0300

    Add files via upload

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,32 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
+const adminMiddleware = (req, res, next) => {
   try {
     let token = req.headers.authorization;
 
     if (!token) {
-      return res.status(401).json({ message: "No token provided" });
+      return res.status(401).json({ message: "Access Denied. No token provided." });
     }
 
-   
     token = token.split(" ")[1];
 
-    const decoded = jwt.verify(token, "secretKey");
+   
+   const decoded = jwt.verify(token, process.env.JWT_SECRET || "secretKey");
+
+    
+    const myStrictAdminEmail = "novastore156@gmail.com"; 
+
+   
+    if (decoded.email !== myStrictAdminEmail && decoded.role !== "admin") {
+      return res.status(403).json({ message: "Access Denied. You are not the real Admin!" });
+    }
 
     req.user = decoded;
-
     next();
   } catch (err) {
-    console.log(err);
-    return res.status(401).json({ message: "Invalid token" });
+    console.log("Admin Auth Error:", err);
+    return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
-module.exports = authMiddleware;
+module.exports = adminMiddleware;
